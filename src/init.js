fd7a384d40841e8d0661d1db3c13f55775463520
Dangt293 <danigt935@gmail.com>
2025-05-14 17:46:57 +0200

    Inserción de comentarios indicativos en el código

--- src/init.js
+++ src/init.js
@@ -13,11 +13,13 @@ const config = {
 var game = new Phaser.Game(config);
 
 function preload() {
+    //Carga el sprite del átomo
     this.load.spritesheet("atomo", "assets/AtomoJuego19.png", {
         frameWidth: 96,
         frameHeight: 96
     })
 
+    //Carga el sprite de la partícula radioactiva
     this.load.spritesheet("RadioactivePart", "assets/Radioactive.png", {
         frameWidth: 96,
         frameHeight: 96
@@ -25,6 +27,7 @@ function preload() {
 }
 
 function create() {
+    //Refiere al sprite del átomo
     this.anims.create({
         key: "move",
         frames: this.anims.generateFrameNumbers("atomo", { frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] }),
@@ -34,6 +37,7 @@ function create() {
     this.player = this.add.sprite(200, 350, "atomo")
     this.player.play("move", true)
 
+    //Refiere al sprite de la partícula radioactiva
     this.anims.create({
         key: "walk",
         frames: this.anims.generateFrameNumbers("RadioactivePart", { frames: [0, 1, 2, 3, 4, 5, 6, 7] }),
