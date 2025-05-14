f6889f84adb5d3b396980e32d5b4476e6dd5998a
Dangt293 <danigt935@gmail.com>
2025-05-14 13:26:59 +0200

    Añado el proyecto

---
+++ src/init.js
@@ -0,0 +1,49 @@
+const config = {
+    width: 800,
+    height: 600,
+    parent: "container",
+    type: Phaser.AUTO,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+}
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.spritesheet("atomo", "assets/AtomoJuego19.png", {
+        frameWidth: 96,
+        frameHeight: 96
+    })
+
+    this.load.spritesheet("RadioactivePart", "assets/Radioactive.png", {
+        frameWidth: 96,
+        frameHeight: 96
+    })
+}
+
+function create() {
+    this.anims.create({
+        key: "move",
+        frames: this.anims.generateFrameNumbers("atomo", { frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] }),
+        frameRate: 19,
+        repeat: -1
+    })
+    this.player = this.add.sprite(200, 350, "atomo")
+    this.player.play("move", true)
+
+    this.anims.create({
+        key: "walk",
+        frames: this.anims.generateFrameNumbers("RadioactivePart", { frames: [0, 1, 2, 3, 4, 5, 6, 7] }),
+        frameRate: 9,
+        repeat: -1
+    })
+    this.enemy = this.add.sprite(400, 300, "RadioactivePart")
+    this.enemy.play("walk", true)
+}
+
+function update(time, delta) {
+
+}
