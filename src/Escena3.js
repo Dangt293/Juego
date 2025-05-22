ee2eea645c0361da2ebd8f8dedea911d4f896341
Dangt293 <danigt935@gmail.com>
2025-05-22 10:06:15 +0200

    Últimos ajustes al trabajo

--- src/Escena3.js
+++ src/Escena3.js
@@ -2,7 +2,7 @@ class Escena3 extends Phaser.Scene{
     constructor(){
         super({key : "Scene3"});
     }
-    
+
     preload() {
         this.load.image("Nuclear", "assets/NuclearRapida.png");
 
@@ -10,6 +10,14 @@ class Escena3 extends Phaser.Scene{
             frameWidth: 96,
             frameHeight: 96
         });
+    }
+
+    generarEnemigosDerrota() {
+        let x = Math.floor(Math.random() * (920 - 20 + 1)) + 20;
+
+        let enemigo = this.physics.add.sprite(x, 50, "RadioAct");
+        enemigo.setVelocityY(150); // Caída hacia abajo
+        enemigo.play("rotate"); // Opcional: animación de rotación si tienes spritesheet
     }
 
     create() {
@@ -20,7 +28,7 @@ class Escena3 extends Phaser.Scene{
             repeat: -1
         });
 
-        this.enemy = this.physics.add.sprite(400, 0, "Radioactiveart");
+        this.enemy = this.physics.add.sprite(-111,-111,"Radioactiveart");
         this.enemy.play("rotate", true);
 
         this.add.text(400, 100, 'HAS PERDIDO', {
@@ -28,7 +36,12 @@ class Escena3 extends Phaser.Scene{
             color: '#ffffff'
         }).setOrigin(0.5);
 
-    
+        this.time.addEvent({
+            delay: 3000, 
+            callback: this.generarEnemigosDerrota,
+            callbackScope: this,
+            loop: true
+        });
 
         const playButton = this.add.text(400, 250, 'VOLVER AL INICIO', {
             fontSize: '32px',
@@ -39,6 +52,6 @@ class Escena3 extends Phaser.Scene{
             this.scene.start('Scene1'); 
         });
 
-        
+
     }
 }
--- src/Escena4.js
+++ src/Escena4.js
@@ -3,8 +3,12 @@ class Escena4 extends Phaser.Scene{
         super({key : "Scene4"});
     }
 
+    preload (){
+        this.load.image("Trofeo" , "assets/Trofeo.png")
+    }
+
     create() {
-        
+        this.add.rectangle(0, 0, 1000, 700, 0x3A025B).setOrigin(0,0);
         this.add.text(400, 100, '¡FELICIDADES, GANASTE!', {
             fontSize: '48px',
             color: '#ffffff'
@@ -18,8 +22,10 @@ class Escena4 extends Phaser.Scene{
         playButton.on('pointerdown', () => {
             this.scene.start('Scene1'); 
         });
+        
+        this.add.image(400,350,"Trofeo");
 
         
     }
 
-}
+}s
